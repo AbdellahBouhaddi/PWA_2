258425f0c78b124017820725ab0f72d4eccc879b
Abdllah-did-this <douibouhaddi@gmail.com>
2022-08-01 15:32:29 +0100

    posts crud 2
    ajouter lire modifier supprimer  un post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,7 +2,7 @@ const postModel = require('../models/post.model')
 const PostModel = require('../models/post.model')
 const UserModel = require('../models/user.models')
 
-const ObjectID = require('mongoose').Types.ObjectID
+const ObjectID = require('mongoose').Types.ObjectId
 
 module.exports.readPost = (req, res) => {
   postModel.find((err, docs) => {
@@ -26,5 +26,39 @@ module.exports.createPost = async (req, res) => {
     return res.status(400).send(err)
   }
 }
-module.exports.updatePost = (req, res) => {}
-module.exports.deletePost = (req, res) => {}
+module.exports.updatePost = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send('ID unknown :' + req.params.id)
+
+  const updatedRecord = {
+    message: req.body.message,
+  }
+  PostModel.findByIdAndUpdate(
+    req.params.id,
+    { $set: updatedRecord },
+    { new: true },
+    (err, docs) => {
+      if (!err) res.send(docs)
+      else console.log('update error :' + err)
+    }
+  )
+}
+module.exports.deletePost = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send('ID unknown :' + req.params.id)
+
+  postModel.findByIdAndRemove(req.params.id, (err, docs) => {
+    if (!err) res.send(docs)
+    else console.log('delete error :' + err)
+  })
+}
+
+module.exports.likePost = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send('ID unknown :' + req.params.id)
+}
+
+module.exports.unlikePost = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send('ID unknown :' + req.params.id)
+}
