3d4aea80a982b0785e67d1b8c3269d8d88f05c4e
Abdllah-did-this <douibouhaddi@gmail.com>
2022-08-31 20:34:58 +0100

    therd

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
 }
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'))
 
-  app.get('*', (req, res) => {
+  app.get('*', checkUser, (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
