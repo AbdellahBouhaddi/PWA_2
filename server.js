db43373c0000c1525014c7761b15f3fd3df9ee0c
Abdllah-did-this <douibouhaddi@gmail.com>
2022-08-31 22:33:15 +0100

    firs deploygg  lllin

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static(path.join('client/build')))
 
-  app.get('*', (req, res) => {
+  app.get('*', checkUser, (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
