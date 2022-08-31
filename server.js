a8af4c079f87e95fa127aa0db3734ccb9e5c08ab
Abdllah-did-this <douibouhaddi@gmail.com>
2022-08-31 20:46:44 +0100

    therd 5

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ app.use('/api/post', postRoutes)
 // serveur production assests
 if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('client/build'))
+  app.use(express.static(path.join('client/build')))
 
-  app.get('*', checkUser, (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
